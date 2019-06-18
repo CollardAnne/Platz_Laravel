0cbd5f5766b6441bb5b5d4412ff101802c95a6d4
Collard A <collard.anne.j@gmail.com>
2019-06-18 09:13:11 +0200

    Fix bug : Dropdown menu

--- platz/app/Http/Controllers/Auth/VerificationController.php
+++ platz/app/Http/Controllers/Auth/VerificationController.php
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/e';
 
     /**
      * Create a new controller instance.
